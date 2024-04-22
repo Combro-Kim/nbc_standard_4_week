9787cb5a638958adee62cfcb0c011ab1df320473
Combro-Kim <rlawog@nate.com>
2024-04-22 13:55:37 +0900

    commit 연습

--- app/src/main/java/com/example/nbc_standard_4_week/presentation/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/nbc_standard_4_week/presentation/detail/DetailViewModel.kt
@@ -14,7 +14,7 @@ class DetailViewModel : ViewModel() {
         return dataList.find { it.id == id }
     }
 
-
+    //commit 연습
     //LiveData 사용
     //단순 받아온 데이터를 보여주기 때문에 LiveData 사용 안함
 /*    private val _dataList: MutableLiveData<List<Data>> = MutableLiveData()
