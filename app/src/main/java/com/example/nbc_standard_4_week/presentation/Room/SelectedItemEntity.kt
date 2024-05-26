cb5b50b491e8ad1672e0770eebe73fbc332bbf6b
Combro-Kim <rlawog@nate.com>
2024-05-26 22:17:37 +0900

    for push

--- app/src/main/java/com/example/nbc_standard_4_week/presentation/Room/SelectedItemEntity.kt
+++ app/src/main/java/com/example/nbc_standard_4_week/presentation/Room/SelectedItemEntity.kt
@@ -1,13 +1,18 @@
-//package com.example.nbc_standard_4_week.presentation.myStorage.Room
-//
-//import androidx.room.Entity
-//import androidx.room.PrimaryKey
-//
-//@Entity(tableName = "selected_items")
-//data class SelectedItemEntity(
-//    @PrimaryKey val id: Int,
-//    val login: String,
-//    val htmlUrl: String,
-//    val avatarUrl: String,
-//    var isLiked: Boolean = false
-//)
+/*
+package com.example.nbc_standard_4_week.presentation.myStorage.Room
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "selected_items")
+@Parcelize
+data class SelectedItemEntity(
+    @PrimaryKey(autoGenerate = true)
+    val uid: Int = 0,
+    val login: String,
+    val htmlUrl: String,
+    val avatarUrl: String,
+    var isLiked: Boolean = false
+): Parcelable*/
